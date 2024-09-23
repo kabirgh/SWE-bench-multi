4e049fb799817dba360790f23af0f4511d47b291
Kabir Khandpur <kabirk@live.com>
2024-09-23 17:58:21 +0000

    test_input.txt -> input.txt

--- swebench/scripts/test_log_parser.py
+++ swebench/scripts/test_log_parser.py
@@ -1,12 +1,11 @@
-from collections import defaultdict
 import json
 from swebench.harness.constants import TestStatus
-from swebench.harness.adapters.adapter import (
-    tap_log_parser as log_parser,
+from swebench.harness.adapters.javascript_adapter import (
+    vitest_log_parser as log_parser,
 )
 
 if __name__ == "__main__":
-    with open("swebench/scripts/test_input.txt", "r") as f:
+    with open("swebench/scripts/input.txt", "r") as f:
         log = f.read()
 
     test_status_map = log_parser(log)
@@ -14,16 +13,11 @@ if __name__ == "__main__":
     results = {
         "FAIL_TO_PASS": [],
         "PASS_TO_PASS": [],
-        "FAIL_TO_FAIL": [],
     }
 
     for test, status in test_status_map.items():
         if status == TestStatus.FAILED.value:
-            # only for three.js, should be commented out for other log formats
-            if "# TODO" in test:
-                results["FAIL_TO_FAIL"].append(test)
-            else:
-                results["FAIL_TO_PASS"].append(test)
+            results["FAIL_TO_PASS"].append(test)
         elif status == TestStatus.PASSED.value:
             results["PASS_TO_PASS"].append(test)
 
